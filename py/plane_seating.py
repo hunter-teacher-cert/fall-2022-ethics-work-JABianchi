b656b0024395d103b01f19ff6e8c8f781e8ea0b2
Joel Andrew Bianchi <JABianchi@gmail.com>
2022-11-21 05:33:24 -0500

    Plane Seating updated

--- py/plane_seating.py
+++ py/plane_seating.py
@@ -1,3 +1,5 @@
+# Original file.  See updated file in week09 folder
+
 """This program simulates the sales of tickets for a specific flight.
 
 A plane is represented by a list. Each element of the list is a row in
